186ac502a6420f430e86b0c0240ff3cb4c893d49
keithwachira <keithwachira@gmail.com>
2018-09-12 08:28:46 +0300

    added notifications

--- src/App.vue
+++ src/App.vue
@@ -111,6 +111,45 @@
 
       </v-spacer>
 
+
+      <v-spacer></v-spacer>
+
+
+      <v-menu offset-y origin="center center" class="elelvation-1" :nudge-bottom="14" transition="scale-transition">
+        <v-btn icon flat slot="activator">
+          <v-badge color="red" overlap>
+            <span slot="badge">3</span>
+            <v-icon medium>notifications</v-icon>
+          </v-badge>
+        </v-btn>
+        <notification-list>
+
+        </notification-list>
+
+      </v-menu>
+
+
+
+
+      <v-menu offset-y origin="center center" :nudge-bottom="10" transition="scale-transition">
+        <v-btn icon large flat slot="activator">
+          <v-avatar size="30px">
+            <img src="https://avatars2.githubusercontent.com/u/8012032?s=460&v=4" alt="Michael Wang"/>
+          </v-avatar>
+        </v-btn>
+        <v-list class="pa-0">
+          <v-list-tile v-for="(item,index) in item" :to="!item.href ? { name: item.name } : null" :href="item.href" @click="item.click" ripple="ripple" :disabled="item.disabled" :target="item.target" rel="noopener" :key="index">
+            <v-list-tile-action v-if="item.icon">
+              <v-icon>{{ item.icon }}</v-icon>
+            </v-list-tile-action>
+            <v-list-tile-content>
+              <v-list-tile-title>{{ item.title }}</v-list-tile-title>
+            </v-list-tile-content>
+          </v-list-tile>
+        </v-list>
+      </v-menu>
+
+
     </v-toolbar>
     <v-content>
       <router-view/>
@@ -123,7 +162,13 @@
 </template>
 
 <script>
+
+  import NotificationList from "../src/components/notifications/NotificationList";
   export default {
+    components:{
+
+      NotificationList
+    },
     data() {
       return {
         clipped: true,
@@ -168,6 +213,22 @@
             to: '/editor'
 
           },
+
+
+          {
+            action: 'note',
+            title: 'Components',
+
+            items: [
+              {title: 'Cards', to: '/admin/users/allusers'},
+              {title: 'Sign Up', to: '/admin/users/allusers'},
+              {title: '404', to: '/admin/users/allusers'},
+              {title: '403', to: '/admin/users/blocked'},
+              {title: '500', to: '/admin/users/blocked'},
+
+            ]
+          },
+
 
 
 

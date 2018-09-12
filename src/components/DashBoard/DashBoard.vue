17d4ae908af175e8cf0f14435e67207d1f39aa54
keithwachira <keithwachira@gmail.com>
2018-09-12 06:39:36 +0300

    initial commit

---
+++ src/components/DashBoard/DashBoard.vue
@@ -0,0 +1,25 @@
+<template>
+<v-container>
+  <v-layout>
+
+    DashBoard Page
+
+  </v-layout>
+
+
+
+</v-container>
+</template>
+
+<script>
+
+
+    export default {
+
+      name: "dash-board"
+    }
+</script>
+
+<style scoped>
+
+</style>
